c383749c233bd43ed128aac9a6bf0635762fecf5
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-10 12:51:58 -0800

    woret second test

--- __tests__/people.test.js
+++ __tests__/people.test.js
@@ -1,26 +1,58 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const { request } = require('../lib/app');
+const request = require('supertest');
 const app = require('../lib/app');
-
+const { People } = require('../lib/models/peopleModel');
 
 describe('backend-express-template routes', () => {
-    beforeEach(() => {
-        return setup(pool);
-    });
+  beforeEach(() => {
+    return setup(pool);
+  });
 
+  it('/people should render a list of people', async () => {
+    const resp = await request(app).get('/people');
+    const expected = await People.getAllPeople();
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "first_name": "Udale",
+          "id": "1",
+          "last_name": "Baggelley",
+        },
+        Object {
+          "first_name": "Lorilee",
+          "id": "2",
+          "last_name": "Whittam",
+        },
+        Object {
+          "first_name": "Jasun",
+          "id": "3",
+          "last_name": "Yerson",
+        },
+        Object {
+          "first_name": "Bridget",
+          "id": "4",
+          "last_name": "Bettesworth",
+        },
+        Object {
+          "first_name": "Beth",
+          "id": "5",
+          "last_name": "Billany",
+        },
+      ]
+    `);
+  });
 
-
-    it('/people should render a list of people', async () => {
-        const resp = await request(app).get('/people');
-        expect(resp).toBe(200);
-    });
-
-
-
-
-
-    afterAll(() => {
-        pool.end();
-    });
+    
+    it('/:id should bring back one person', async () => {
+        const resp = await request(app).get('/people/1');
+        expect(resp.status).toBe(200);
+    })
+    
+    
+    
+    
+  afterAll(() => {
+    pool.end();
+  });
 });
