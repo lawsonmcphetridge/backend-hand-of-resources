670807814eb7f25bdd58c92ac28503241fe4c0c0
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-14 11:54:25 -0800

    passed first shoe test

--- __tests__/shoe.test.js
+++ __tests__/shoe.test.js
@@ -3,7 +3,6 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-
 describe('tests for dog route', () => {
   beforeEach(() => {
     return setup(pool);
@@ -11,11 +10,37 @@ describe('tests for dog route', () => {
 
   it('/ should render a list of shoes', async () => {
     const resp = await request(app).get('/shoes');
-    expect(resp.body).toMatchInlineSnapshot();
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "brand": "dunk",
+          "id": "1",
+          "name": "nike",
+        },
+        Object {
+          "brand": "slides",
+          "id": "2",
+          "name": "yeezy",
+        },
+        Object {
+          "brand": "SwiftRun",
+          "id": "3",
+          "name": "adidas",
+        },
+        Object {
+          "brand": "zebra",
+          "id": "4",
+          "name": "yeezy",
+        },
+        Object {
+          "brand": "vans",
+          "id": "5",
+          "name": "classic",
+        },
+      ]
+    `);
   });
-    
-    
-    
+
   afterAll(() => {
     pool.end();
   });
