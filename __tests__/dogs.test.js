1126bafbe774d4859dbda4ff41fed4c0cbd0c089
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-14 09:08:46 -0800

    wrote test for dog list

---
+++ __tests__/dogs.test.js
@@ -0,0 +1,21 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('tests for dog route', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('/dogs should render a list of dogs', () => {
+    
+  });
+
+
+
+
+  afterAll(() => {
+    pool.end();
+  });
+});
