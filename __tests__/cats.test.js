ed0a2cfa82898d4d17a00e2e6527906fe88f6659
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-14 15:06:33 -0800

    passed first test

--- __tests__/cats.test.js
+++ __tests__/cats.test.js
@@ -10,12 +10,36 @@ describe('tests for dog route', () => {
 
   it('/dogs should render a list of cats', async () => {
     const resp = await request(app).get('/cat');
-    expect(resp.body).toMatchInlineSnapshot();
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "breed": "Siamese",
+          "id": "1",
+          "name": "Udo",
+        },
+        Object {
+          "breed": "Persian",
+          "id": "2",
+          "name": "Evelina",
+        },
+        Object {
+          "breed": "Ragdoll",
+          "id": "3",
+          "name": "Chen",
+        },
+        Object {
+          "breed": "Sphynx",
+          "id": "4",
+          "name": "Vilhelmi",
+        },
+        Object {
+          "breed": "Birman",
+          "id": "5",
+          "name": "Laurentino",
+        },
+      ]
+    `);
   });
-    
-    
-    
-    
 
   afterAll(() => {
     pool.end();
