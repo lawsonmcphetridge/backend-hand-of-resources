14b86afb73ccbf047433b514c6c7cc3046d3190b
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-14 09:17:40 -0800

    added sql for dogs

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS dog;
 
 CREATE TABLE people (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -6,16 +7,35 @@ CREATE TABLE people (
     last_name VARCHAR
 );
 
+CREATE TABLE dog (
+id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+name VARCHAR,
+breed VARCHAR
+);
+
 
 INSERT INTO people (
     first_name,
     last_name
 )
-
 VALUES
 ('Udale', 'Baggelley'),
 ('Lorilee', 'Whittam'),
 ('Jasun', 'Yerson'),
 ('Bridget', 'Bettesworth'),
 ('Beth', 'Billany')
+;
+
+INSERT INTO dog (
+    name,
+    breed
+)
+
+VALUES
+('Hendricus', 'Golden'),
+('Freyr', 'German'),
+('Heimir', 'Poodle'),
+('Helios', 'Bulldog'),
+('Berna', 'Husky')
+
 ;
