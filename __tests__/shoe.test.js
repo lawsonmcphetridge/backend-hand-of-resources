ef4590ec081aecb801bf5bb670db034f4da7a095
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-14 11:45:00 -0800

    added test for shoes route

---
+++ __tests__/shoe.test.js
@@ -0,0 +1,22 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+describe('tests for dog route', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('/ should render a list of shoes', async () => {
+      const resp = await request(app).get('/shoes');
+      expect(resp.body).toMatchInlineSnapshot();
+  });
+    
+    
+    
+  afterAll(() => {
+    pool.end();
+  });
+});
