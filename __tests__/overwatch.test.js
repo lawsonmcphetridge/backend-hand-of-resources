c124a043e93875a21922adc356852307d0932ce6
Lawson <lawsonmcphetridge2020@gmail.com>
2022-11-14 10:52:54 -0800

    added test for single char

--- __tests__/overwatch.test.js
+++ __tests__/overwatch.test.js
@@ -10,9 +10,42 @@ describe('tests for dog route', () => {
 
   it('/overwatch should render a list of characters', async () => {
     const resp = await request(app).get('/overwatch');
-    expect(resp.body).toMatchInlineSnapshot();
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "id": "1",
+          "name": "Mercy",
+          "role": "Healer",
+        },
+        Object {
+          "id": "2",
+          "name": "DoomFist",
+          "role": "Tank",
+        },
+        Object {
+          "id": "3",
+          "name": "Mcree",
+          "role": "Dps",
+        },
+        Object {
+          "id": "4",
+          "name": "Tracer",
+          "role": "Dps",
+        },
+        Object {
+          "id": "5",
+          "name": "Roadhog",
+          "role": "Tank",
+        },
+      ]
+    `);
   });
 
+  it('/overwatch/1 should render a single char', async (req, res) => {
+    const resp = await request(app).get('/overwatch/1');
+  });
+    
+    
   afterAll(() => {
     pool.end();
   });
